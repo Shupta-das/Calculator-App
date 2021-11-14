10737f8ace561547ed540bd287f310eb7054896a
Shupta-das <shuptadas25203@gmail.com>
2021-11-14 16:57:50 +0600

    added new class shuptadas

---
+++ app/src/main/java/com/example/calculator/shuptadas.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+public class shuptadas {
+    int x ;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public shuptadas(int x) {
+        this.x = x;
+    }
+}
